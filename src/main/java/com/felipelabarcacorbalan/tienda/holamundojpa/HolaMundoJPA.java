f852c07a6cb29d36dfc1950cdbe61fff5a1e6a5a
AutorIntelectual <felipe.labarca.c@gmail.com>
2023-11-01 15:33:04 -0300

    first commit, POM configured

---
+++ src/main/java/com/felipelabarcacorbalan/tienda/holamundojpa/HolaMundoJPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.felipelabarcacorbalan.tienda.holamundojpa;
+
+/**
+ *
+ * @author NB-FLABARCA
+ */
+public class HolaMundoJPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
